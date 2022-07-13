e2ece20a567a10d3412f93a560c4b259567477c1
alanuadalupe <alanuadalupe1665@gmail.com>
2022-07-13 14:40:34 -0500

    guardar para cambiar de fecha

--- src/Components/routes/WatchesRoutes.js
+++ src/Components/routes/WatchesRoutes.js
@@ -1,33 +1,11 @@
-import { Outlet, Route, Router } from "react-router-dom";
 import useAxios from "../Atoms/getAxios";
-import NewShopAllNewArrivalsTwo from "../Pages/Relojes/NewShopAllNewArrivalsTwo";
 const WatchesRoutes = ({
 relojes
 }) => {
 
-    const [data,error ] = useAxios(relojes);
-
-    console.log(data)
 
     return(
-        <Router>
-            {
-                data ? 
-                data.map(item => {
-                    return(
-                        <Route path={item.name} 
-                        element={<NewShopAllNewArrivalsTwo 
-                            direccion={"/WATCHES/BEST SELLERS"}
-                            relojes={'http://localhost:5000/relojestotalroute/all'}
-                            name={'TRENDING'}
-                        />}
-                        />
-                    )
-                })
-                : 
-                null
-            }
-        </Router>
+        <h1>WatchesRoutes</h1>
     )
 }
 
